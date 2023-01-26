d83aad9c67fcb843510b70a2942ed4acb92d4e5c
yihshe <yihshe@student.ethz.ch>
2023-01-26 10:37:09 +0000

    query h5 file

---
+++ query_h5.py
@@ -0,0 +1,30 @@
+import h5py
+import os
+import numpy as np
+import pandas as pd
+
+data_path = "/maps/forecol/data/GEDI/level2B"
+files = [f for f in os.listdir(data_path) if f.endswith(".h5")]
+
+# TODO no time info, granule['METADATA']['DatasetIdentification'] is empty
+# data = {
+#     "pai": np.array([]),
+#     "l2b_quality_flag": np.array([]),
+#     "shot_number": np.array([]),
+#     "lon": np.array([]),
+#     "lat": np.array([]),
+# }
+
+data = dict({})
+for f in files:
+    fname = os.path.join(data_path, f)
+    granule = h5py.File(fname, "r")
+    # TODO which beam to use and for each beam, which shots to use?
+    filter = np.array(granule['BEAM0000']['l2b_quality_flag'])==1
+    data["pai"] = np.array(granule['BEAM0000']['pai'])[filter]
+    data["l2b_quality_flag"] = np.array(granule['BEAM0000']['l2b_quality_flag'])[filter]
+    data["shot_number"] = np.array(granule['BEAM0000']['geolocation']['shot_number'])[filter]
+    data["lon"] = np.array(granule['BEAM0000']['geolocation']['lon_lowestmode'])[filter]
+    data["lat"] = np.array(granule['BEAM0000']['geolocation']['lat_lowestmode'])[filter]
+    # NOTE the following line can be used to save the data as a csv file
+    df = pd.DataFrame(data)
---
+++ run.sh
@@ -0,0 +1,3 @@
+# for GEDI query
+# python3 -m pdb query_psql.py
+python3 -m pdb query_h5.py
