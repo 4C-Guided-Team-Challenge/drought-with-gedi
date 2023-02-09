6ed7d73286e64889b2832878d542109510f0a2f0
mahuna13 <jovana.p.knezevic@gmail.com>
2023-02-09 10:54:23 +0000

    fixes linting errors

--- drought/visualization/ee_viz.py
+++ drought/visualization/ee_viz.py
@@ -6,7 +6,8 @@ from drought.data.ee_converter import gdf_to_ee_polygon
 from drought.data.pipeline import get_gpd_polygons
 
 
-def viz_all_polygons(img: ee.Image,  band: str):
+def viz_all_polygons(img: ee.Image,  band: str) -> folium.Map:
+    ''' Plots image pixels on a map across all 8 polygons. '''
     polygons = get_gpd_polygons().geometry
     lon, lat = polygons.centroid.x.median(), polygons.centroid.y.median()
     my_map = folium.Map(location=[lat, lon], zoom_start=5, control_scale=True)
@@ -25,10 +26,12 @@ def viz_all_polygons(img: ee.Image,  band: str):
                             {'min': band_min, 'max': band_max,
                              'palette': colors}, name=band)
 
-    display(my_map)
+    return my_map
 
 
-def viz_single_polygon(img: ee.Image, polygon_index: int,  band: str):
+def viz_single_polygon(img: ee.Image, polygon_index: int,  band: str) \
+        -> folium.Map:
+    ''' Plots image pixels on a map clipped to a specified polygon. '''
     polygon = get_gpd_polygons().geometry[polygon_index]
     region = gdf_to_ee_polygon(polygon)
     img_stats = geemap.image_stats(img.clip(region), scale=5000).getInfo()
@@ -49,11 +52,11 @@ def viz_single_polygon(img: ee.Image, polygon_index: int,  band: str):
     colormap = branca.colormap.LinearColormap(
         colors=['yellow', 'red']).scale(band_min, band_max)
     colormap.add_to(my_map)
-    display(my_map)
+    return my_map
 
 
 def add_ee_layer(self, ee_image_object, vis_params, name):
-    """Adds a method for displaying Earth Engine image tiles to folium map."""
+    '''Adds a method for displaying Earth Engine image tiles to folium map.'''
     map_id_dict = ee.Image(ee_image_object).getMapId(vis_params)
     folium.raster_layers.TileLayer(
         tiles=map_id_dict['tile_fetcher'].url_format,
