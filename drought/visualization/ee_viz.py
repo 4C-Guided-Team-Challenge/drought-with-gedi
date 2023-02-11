ddc4e362bafea104d9d482262d7c986a4bd6c4c4
Felipe Begliomini <fnb25@cam.ac.uk>
2023-02-11 16:42:32 +0000

    Added possibility to inform user's band names

--- drought/visualization/ee_viz.py
+++ drought/visualization/ee_viz.py
@@ -6,6 +6,7 @@ from drought.data.pipeline import get_gpd_polygons, get_ee_polygons
 
 
 def viz_all_polygons(img: ee.Image, bands: list[str],
+                     band_names: list[str] = None,
                      palette: cm.Box = cm.palettes.inferno_r,
                      scale: int = 5000, map: geemap.Map = None) -> geemap.Map:
     '''
@@ -16,12 +17,6 @@ def viz_all_polygons(img: ee.Image, bands: list[str],
     '''
     polygons = get_gpd_polygons().geometry
     ee_polygons = get_ee_polygons()
-
-    if map is None:
-        map = geemap.Map(center=(polygons.centroid.y.median(),
-                                 polygons.centroid.x.median()),
-                         zoom=5)
-
     # Clip image to include all of the polygons.
     clipped = ee.Image(ee.ImageCollection(
         [img.clip(geometry) for geometry in ee_polygons]).mosaic())
@@ -29,19 +24,27 @@ def viz_all_polygons(img: ee.Image, bands: list[str],
     img_stats = [geemap.image_stats(img.clip(region), scale=scale).getInfo()
                  for region in ee_polygons]
 
-    for band in bands:
+    if map is None:
+        map = geemap.Map(center=(polygons.centroid.y.median(),
+                                 polygons.centroid.x.median()),
+                         zoom=5)
+    if band_names is None:
+        band_names = bands
+
+    for band, name in zip(bands, band_names):
         # Calculate min and max across all polygons.
         band_min = min(stat['min'][band] for stat in img_stats)
         band_max = max(stat['max'][band] for stat in img_stats)
         band_vis = {'bands': band, 'palette': palette,
                     'min': band_min, 'max': band_max}
-        map.addLayer(clipped, band_vis, band)
-        map.add_colorbar(band_vis, label=band, layer_name=band)
+        map.addLayer(clipped, band_vis, name)
+        map.add_colorbar(band_vis, label=name, layer_name=band)
 
     return map
 
 
 def viz_single_polygon(img: ee.Image, polygon_index: int,  bands: list[str],
+                       band_names: list[str] = None,
                        palette: cm.Box = cm.palettes.inferno_r,
                        scale: int = 5000,
                        map: geemap.Map = None) -> geemap.Map:
@@ -53,18 +56,21 @@ def viz_single_polygon(img: ee.Image, polygon_index: int,  bands: list[str],
     '''
     polygon = get_gpd_polygons().geometry[polygon_index]
     region = gdf_to_ee_polygon(polygon)
-    img_stats = geemap.image_stats(img.clip(region), scale=5000).getInfo()
+    img_stats = geemap.image_stats(img.clip(region), scale=scale).getInfo()
+
+    if band_names is None:
+        band_names = bands
 
     if map is None:
         map = geemap.Map(center=(polygon.centroid.y, polygon.centroid.x),
                          zoom=9)
 
-    for band in bands:
+    for band, name in zip(bands, band_names):
         band_min = img_stats['min'][band]
         band_max = img_stats['max'][band]
         band_vis = {'bands': band, 'palette': palette,
                     'min': band_min, 'max': band_max}
-        map.addLayer(img.clip(region), band_vis, band)
-        map.add_colorbar(band_vis, label=band, layer_name=band)
+        map.addLayer(img.clip(region), band_vis, name)
+        map.add_colorbar(band_vis, label=name, layer_name=band)
 
     return map
