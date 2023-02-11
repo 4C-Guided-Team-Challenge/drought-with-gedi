45383fdb974c48893713636e952d2460ce189995
mahuna13 <jovana.p.knezevic@gmail.com>
2023-02-11 14:59:40 +0000

    Addresses code review feedback. Uses geemap instead of folium

--- drought/visualization/ee_viz.py
+++ drought/visualization/ee_viz.py
@@ -1,71 +1,70 @@
-import branca
 import ee
-import folium
 import geemap
+import geemap.colormaps as cm
 from drought.data.ee_converter import gdf_to_ee_polygon
-from drought.data.pipeline import get_gpd_polygons
+from drought.data.pipeline import get_gpd_polygons, get_ee_polygons
 
 
-def viz_all_polygons(img: ee.Image,  band: str) -> folium.Map:
-    ''' Plots image pixels on a map across all 8 polygons. '''
+def viz_all_polygons(img: ee.Image, bands: list[str],
+                     palette: cm.Box = cm.palettes.inferno_r,
+                     scale: int = 5000, map: geemap.Map = None) -> geemap.Map:
+    '''
+    Plots image on a map across all 8 polygons.
+
+    Each band is added as a separate layer on the map.
+
+    '''
     polygons = get_gpd_polygons().geometry
-    lon, lat = polygons.centroid.x.median(), polygons.centroid.y.median()
-    my_map = folium.Map(location=[lat, lon], zoom_start=5, control_scale=True)
+    ee_polygons = get_ee_polygons()
 
-    for polygon in polygons:
-        region = gdf_to_ee_polygon(polygon)
-        img_stats = geemap.image_stats(img.clip(region), scale=5000).getInfo()
+    if map is None:
+        map = geemap.Map(center=(polygons.centroid.y.median(),
+                                 polygons.centroid.x.median()),
+                         zoom=5)
 
-        band_min = img_stats['min'][band]
-        band_max = img_stats['max'][band]
-        if band_min == band_max:
-            band_max = band_min + 1
-        colors = ['#ffff00', '#800026']
+    # Clip image to include all of the polygons.
+    clipped = ee.Image(ee.ImageCollection(
+        [img.clip(geometry) for geometry in ee_polygons]).mosaic())
+    # Get image stats for each region.
+    img_stats = [geemap.image_stats(img.clip(region), scale=scale).getInfo()
+                 for region in ee_polygons]
 
-        my_map.add_ee_layer(img.select(band).clip(region),
-                            {'min': band_min, 'max': band_max,
-                             'palette': colors}, name=band)
+    for band in bands:
+        # Calculate min and max across all polygons.
+        band_min = min(stat['min'][band] for stat in img_stats)
+        band_max = max(stat['max'][band] for stat in img_stats)
+        band_vis = {'bands': band, 'palette': palette,
+                    'min': band_min, 'max': band_max}
+        map.addLayer(clipped, band_vis, band)
+        map.add_colorbar(band_vis, label=band, layer_name=band)
 
-    return my_map
+    return map
 
 
-def viz_single_polygon(img: ee.Image, polygon_index: int,  band: str) \
-        -> folium.Map:
-    ''' Plots image pixels on a map clipped to a specified polygon. '''
+def viz_single_polygon(img: ee.Image, polygon_index: int,  bands: list[str],
+                       palette: cm.Box = cm.palettes.inferno_r,
+                       scale: int = 5000,
+                       map: geemap.Map = None) -> geemap.Map:
+    '''
+    Plots image on a map clipped to a specified polygon.
+
+    Each band is added as a separate layer on the map.
+
+    '''
     polygon = get_gpd_polygons().geometry[polygon_index]
     region = gdf_to_ee_polygon(polygon)
     img_stats = geemap.image_stats(img.clip(region), scale=5000).getInfo()
 
-    band_min = img_stats['min'][band]
-    band_max = img_stats['max'][band]
-    if band_min == band_max:
-        band_max = band_min + 1
-    colors = ['#ffff00', '#800026']
+    if map is None:
+        map = geemap.Map(center=(polygon.centroid.y, polygon.centroid.x),
+                         zoom=9)
 
-    lon, lat = polygon.centroid.x, polygon.centroid.y
-    my_map = folium.Map(location=[lat, lon], zoom_start=9, control_scale=True)
-    my_map.add_ee_layer(img.select(band).clip(region),
-                        {'min': band_min, 'max': band_max,
-                         'palette': colors}, name=band)
+    for band in bands:
+        band_min = img_stats['min'][band]
+        band_max = img_stats['max'][band]
+        band_vis = {'bands': band, 'palette': palette,
+                    'min': band_min, 'max': band_max}
+        map.addLayer(img.clip(region), band_vis, band)
+        map.add_colorbar(band_vis, label=band, layer_name=band)
 
-    # Add legend
-    colormap = branca.colormap.LinearColormap(
-        colors=['yellow', 'red']).scale(band_min, band_max)
-    colormap.add_to(my_map)
-    return my_map
-
-
-def add_ee_layer(self, ee_image_object, vis_params, name):
-    '''Adds a method for displaying Earth Engine image tiles to folium map.'''
-    map_id_dict = ee.Image(ee_image_object).getMapId(vis_params)
-    folium.raster_layers.TileLayer(
-        tiles=map_id_dict['tile_fetcher'].url_format,
-        attr='Map Data &copy; <a href="https://earthengine.google.com/">Google Earth Engine</a>',  # noqa: E501
-        name=name,
-        overlay=True,
-        control=True
-    ).add_to(self)
-
-
-# Add Earth Engine drawing method to folium.
-folium.Map.add_ee_layer = add_ee_layer
+    return map
