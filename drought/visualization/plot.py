17e6c45a3deb10a4217e05adfe5382a16cdf47e1
mahuna13 <jovana.p.knezevic@gmail.com>
2023-02-28 11:23:14 +0000

    Saves another version of climate data with P-ET and P-PET calculations. Prototyping seasonality plotting and calculations in progress.

--- drought/visualization/plot.py
+++ drought/visualization/plot.py
@@ -95,3 +95,26 @@ def catplot_per_polygon(data: pd.DataFrame, x, y, hue, kind, bands, title,
         # Plot.
         cat = sns.catplot(x=x, y=y, hue=hue, data=melt, kind=kind, aspect=3)
         cat.fig.suptitle(f"Polygon {polygon_id}", fontsize=15)
+
+
+def plot_per_polygon(data: pd.DataFrame, title, plot_func, fig=None, ax=None, sharey=False
+                     ) -> plt.figure:
+    if fig is None and ax is None:
+        fig, ax = plt.subplots(4, 2, figsize=(30, 24), sharey=sharey)
+        fig.suptitle(title, fontsize=30)
+        fig.tight_layout(pad=3.0, h_pad=4.0, w_pad=8.0)
+
+    for i in range(8):
+        subplot = ax[i // 2, i % 2]
+
+        # Select polygon from the data. Polygon IDs go from 1 to 8.
+        polygon_id = i + 1
+        polygon_data = data[data['polygon_id'] == polygon_id]
+
+        # Plot.
+        plot_func(polygon_data, subplot)
+        #sns.barplot(polygon_data, x=x, y=y, color=color, ax=subplot)
+        subplot.set_title(f"Polygon {polygon_id}", fontsize=20)
+        # subplot.set_ylabel(ylabel)
+
+    return fig, ax
