f96d8ac606bd1e1917bf232cf756f28e101105ff
Felipe Begliomini <fnb25@cam.ac.uk>
2023-03-01 10:42:43 +0000

    Changes and tests, no major advance

--- drought/data/extract_from_ee_image.py
+++ drought/data/extract_from_ee_image.py
@@ -1,17 +1,27 @@
 # %%
 import ee
-import numpy as np
 import pandas as pd
 from drought.data.ee_converter import ee_points_converter
+import geopandas as gpd
+import geemap
+
 ee.Initialize()
 
 
-df = pd.read_feather('/home/fnb25/reduced_gedi.feather').set_index('index')
+df = pd.read_feather('/maps/drought-with-gedi/Felipe/reduced_gedi.feather').set_index('index') # noqa
 
+polygons = gpd.read_file('/home/fnb25/drought-with-gedi/data/polygons/Amazonia_drought_gradient_polygons.shp') # noqa
+
+xmin, ymin, xmax, ymax = polygons.total_bounds
+
+ee_geometry = ee.Geometry.BBox(xmin, ymin, xmax, ymax)
+
+Map = geemap.Map()
 
 # %%
 
-def create_ee_points_series(df: pd.DataFrame):
+
+def create_ee_points_series(df: pd.DataFrame) -> pd.Series:
     """
     Creates a series with ee_points using lat and long
     values from pandas.DataFrame.
@@ -26,8 +36,12 @@ def create_ee_points_series(df: pd.DataFrame):
 # %%
 
 
-ee_list_coords = ee.List(list(zip(df['lon_lowestmode'].iloc[0:1000000],
-                                  df['lat_lowestmode'].iloc[0:1000000])))
+landsat = ee.ImageCollection("LANDSAT/LC09/C02/T1_L2").median()
+
+# %%
+
+ee_list_coords = ee.List(list(zip(df['lon_lowestmode'].iloc[0:100],
+                                  df['lat_lowestmode'].iloc[0:100])))
 
 ee_point = ee_list_coords.map(lambda row: ee.Feature(ee.Geometry.Point(row)))
 
@@ -37,11 +51,11 @@ collection = ee.ImageCollection('MODIS/061/MOD15A2H') \
                   .filterDate('2019-01-01', '2019-10-01') \
                   .mean()
 
-n = 3
+n = 1
 
 weights = [[1] * n] * n
 
-window = kernel = ee.Kernel.fixed(n, n, weights)
+window = ee.Kernel.fixed(n, n, weights)
 
 scale = collection.projection().nominalScale()
 
@@ -56,7 +70,7 @@ values = sample.aggregate_array('Fpar_500m_median')
 
 values2 = sample_pure.aggregate_array('Fpar_500m')
 
-print(values.getInfo())
+tete = values.getInfo()
 
 
 # %%
@@ -66,7 +80,7 @@ def extract_value_from_eeImage(df: pd.DataFrame,
                                ee_points: pd.Series,
                                image: ee.Image, bands: list[str] = None,
                                scale: int = None,
-                               window_size: int = 0,
+                               window_size: int = 1,
                                reducer: str = 'mean'):
     '''
     Extract points from an ee.Image
@@ -75,7 +89,8 @@ def extract_value_from_eeImage(df: pd.DataFrame,
                 'mean': ee.Reducer.mean(),
                 'median': ee.Reducer.median(),
                 'min': ee.Reducer.min(),
-                'max': ee.Reducer.max()
+                'max': ee.Reducer.max(),
+                'sum': ee.Reducer.sum()
                 }
 
     if scale is None:
@@ -84,27 +99,23 @@ def extract_value_from_eeImage(df: pd.DataFrame,
     if bands is None:
         bands = image.bandNames().getInfo()
 
-    reducer = reducer_dict[reducer]
-
-    data_array = np.zeros((df.shape[0], len(bands)))
-    window = ee.Kernel.square(radius=window_size / 2, units='pixels')
-    neighborhood_array = collection.neighborhoodToArray(window)
-
-    for index, point in enumerate(ee_points):
-        values = []
-        dict_results = neighborhood_array.sample(point, scale=scale).first().getInfo()  # noqa
-        for band in bands:
-            array = np.array(dict_results['properties'][band]).flatten()
-            band_value = reducer(array)
-            values.append(band_value)
-        data_array[index, :] = values
-
-    gpd_df_copy = df.copy()
-    for index, band in enumerate(bands):
-        print(index, band)
-        gpd_df_copy[band] = data_array[:, index]
-
-    return gpd_df_copy
+    reducer_func = reducer_dict[reducer]
+    weights = [[1] * window_size] * window_size
+    kernel = ee.Kernel.fixed(window_size, window_size, weights)
+    neigh_array = image.reduceNeighborhood(reducer=reducer_func,
+                                           kernel=kernel)
+    renamed_bands = [(name + '_' + reducer) for name in bands]
+    sample_points = ee_points.apply(lambda point:
+                                    neigh_array.sample(point, scale=scale))
+    bands_df = pd.DataFrame()
+    for band in renamed_bands:
+        band_value = sample_points.apply(lambda point:
+                                         point.aggregate_array(band).getInfo())
+        bands_df[band] = band_value.values
+    columns_name = list(df.columns) + bands
+    result = pd.concat([df, bands_df], axis=1, ignore_index=True)
+    result.columns = columns_name
+    return result
 
 
 # %%
