991451f6cd277f0bb7efbb7a9fb636a8b9063127
mahuna13 <jovana.p.knezevic@gmail.com>
2023-03-02 12:20:10 +0000

    Adds df_extensions file with helper functions for adding dates to dateframes

---
+++ drought/data/df_extensions.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+
+def add_date_column(df: pd.DataFrame):
+    '''
+    Adds 'date' column to the DataFrame df.
+
+    DataFrame must have columns 'year' and 'month' already.
+    '''
+    df = df.sort_values(by=['year', 'month'])
+
+    df['date'] = pd.to_datetime(df['year'].astype(str) +
+                                df['month'] .astype(str),
+                                format='%Y%m').dt.strftime('%m-%y')
+    return df
+
+
+def add_datetime_column(df: pd.DataFrame):
+    '''
+    Adds 'datetime' column to the DataFrame df.
+
+    DataFrame must have columns 'year' and 'month' already.
+    '''
+    df_copy = df.copy()
+    df_copy['day'] = 1
+    df['datetime'] = pd.to_datetime(df_copy[['month', 'day', 'year']])
+    return df
