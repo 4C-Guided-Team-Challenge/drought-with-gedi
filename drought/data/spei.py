2f4498169c97877d8751e553e36c72abcb495664
Felipe Begliomini <fnb25@cam.ac.uk>
2023-03-05 13:09:44 +0000

    Added crea_spei_geotiff function

--- drought/data/spei.py
+++ drought/data/spei.py
@@ -6,15 +6,82 @@ import numpy as np
 import geopandas as gpd
 import rasterio
 
-PATH_FILE = '/maps-priv/maps/drought-with-gedi/spei_data/spei06.nc'
+PATH_FILE = '/maps-priv/maps/drought-with-gedi/spei_data/spei'
 
-ds = xr.open_dataset(PATH_FILE)
+SAVE_DIRECTORY = '/maps-priv/maps/drought-with-gedi/spei_data/'
 
 shapefile = gpd.read_file('/home/fnb25/drought-with-gedi/data/polygons/Amazonia_drought_gradient_polygons.shp')  # noqa
 
 minx, miny, maxx, maxy = shapefile.total_bounds
 
 # %%
+
+
+def create_spei_geotiff(spei_window: int,
+                        open_dir: str = PATH_FILE,
+                        save_dir: str = SAVE_DIRECTORY,
+                        start_date: str = '01-01-2000',
+                        end_date: str = '01-01-2021'):
+    """
+    Opens the SPEI nc file and create a series of aggregated metrics
+    for a time period. Save the results into a geotiff file.
+    """
+    if open_dir == PATH_FILE:
+        SPEI_PATH = open_dir + str(spei_window) + '.nc'
+    ds_spei = xr.open_dataset(SPEI_PATH)
+    spei = ds_spei.sel(time=slice(start_date, end_date))
+    spei_array = spei['spei'].values
+
+    extreme_drought = np.count_nonzero((-2 >= spei_array), axis=0)
+
+    severe_drought = np.count_nonzero((-1.5 >= spei_array) &
+                                      (spei_array > -2), axis=0)
+
+    moderate_drought = np.count_nonzero((-1 >= spei_array) &
+                                        (spei_array > -1.5), axis=0)
+
+    near_normal = np.count_nonzero((1 > spei_array) &
+                                   (spei_array > -1), axis=0)
+
+    moderate_wet = np.count_nonzero((1.5 > spei_array) &
+                                    (spei_array >= 1), axis=0)
+
+    severe_wet = np.count_nonzero((2 > spei_array) &
+                                  (spei_array >= 1.5), axis=0)
+
+    extreme_wet = np.count_nonzero((2 <= spei_array), axis=0)
+
+    height = 360
+    width = 720
+    x_resolution = 0.5
+    y_resolution = 0.5
+    left, top = -179.75, 89.75
+    transform = rasterio.transform.from_bounds(left, top,
+                                               x_resolution, y_resolution,
+                                               width=width, height=height)
+    dtype = np.float64
+    crs = 'EPSG:4326'
+    count = 7
+
+    GEOTIFF_NAME = save_dir + 'reduced_spei' + str(spei_window) + '.tif'
+
+    with rasterio.open(GEOTIFF_NAME, 'w', driver='GTiff',
+                       width=width, height=height, count=count,
+                       dtype=dtype, crs=crs, transform=transform) as dst:
+
+        dst.write(extreme_drought, 1)
+        dst.write(severe_drought, 1)
+        dst.write(moderate_drought, 1)
+        dst.write(near_normal, 1)
+        dst.write(moderate_wet, 1)
+        dst.write(severe_wet, 1)
+        dst.write(extreme_wet, 1)
+
+# %%
+
+
+ds = xr.open_dataset(PATH_FILE)
+
 cropped_ds = ds.sel(lat=slice(miny-3, maxy+3), lon=slice(minx-3, maxx+3))
 
 '''for dim in ds.dims.values():
